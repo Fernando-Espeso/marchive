c15250d26b36352091e650c877d4b479875a8c7f
Fernando-Espeso <sre.fernando@gamil.com>
2023-02-23 16:31:48 +0100

    yes

--- assets/global.js
+++ assets/global.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function(){
   $(document).ready(function() {
     $('.link').click(function(){
        $(this).toggleClass("active");
-       $(this).find(".drop")toggleClass("droped");
+       $(this).find(".dropdown")toggleClass("droped");
     });
   });
 });
