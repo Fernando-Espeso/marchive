82e12f92e242ae9253d59fc8bebdd8880f149c3a
Fernando-Espeso <sre.fernando@gamil.com>
2023-02-24 13:51:43 +0100

    Update global.js

--- assets/global.js
+++ assets/global.js
@@ -54,31 +54,16 @@ document.addEventListener("DOMContentLoaded", function(){
 
     var mySwiper = new Swiper ('.swiper',
     	{
-        speed:1000,
+        speed:0,
     		direction: 'horizontal',
-    		navigation:
-    		{
-    		  nextEl: '.swiper-button-next',
-    		  prevEl: '.swiper-button-prev',
-    		},
-    		pagination:
-    		{
-    			el: '.swiper-pagination',
-    			dynamicBullets: true,
-    		},
     		zoom: true,
-    		keyboard:
-    		{
-    			enabled: true,
-    			onlyInViewport: false,
-    		},
     		mousewheel:
     		{
     			invert: true,
     		},
         autoplay:
         {
-          delay: 2000,
+          delay: 10,
         },
         loop: true,
     	});
