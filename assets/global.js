bf8f1cfb93be4805bd07fecc5e1497f85ed962f4
Fernando-Espeso <sre.fernando@gamil.com>
2023-02-27 16:49:39 +0100

    yes

--- assets/global.js
+++ assets/global.js
@@ -69,6 +69,44 @@ document.addEventListener("DOMContentLoaded", function(){
     });
 
 
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('.logo').outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250);
+
+    function hasScrolled() {
+        var st = $(this).scrollTop();
+
+        // Make sure they scroll more than delta
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        // If they scrolled down and are past the navbar, add class .nav-up.
+        // This is necessary so you never see what is "behind" the navbar.
+        if (st > lastScrollTop && st > navbarHeight){
+            // Scroll Down
+            $('.plaza').removeClass('nav-down').addClass('nav-up');
+        } else {
+            // Scroll Up
+            if(st + $(window).height() < $(document).height()) {
+                $('.plaza').removeClass('nav-up').addClass('nav-down');
+            }
+        }
+
+        lastScrollTop = st;
+    };
+
     var mySwiper = new Swiper ('.home',
     	{
         speed:0,
@@ -84,18 +122,15 @@ document.addEventListener("DOMContentLoaded", function(){
         loop: true,
     	});
 
-
-      var mySwiper = new Swiper ('.relatedd',
-        {
-          slidesPerView: 1,
-          spaceBetween: 10,
-          speed:0,
-          autoplay:
-          {
-            delay: 5000,
-          },
-          loop: true,
-        });
-
-
+    var mySwiper = new Swiper ('.relatedd',
+      {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        speed:0,
+        autoplay:
+      {
+        delay: 5000,
+      },
+        loop: true,
+      });
   });
