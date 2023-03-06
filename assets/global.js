18d78c897a447f2261d834d83dda1218fb224026
Fernando-Espeso <sre.fernando@gamil.com>
2023-03-06 17:13:52 +0100

    es

--- assets/global.js
+++ assets/global.js
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function(){
     		},
         autoplay:
         {
-        delay: 100,
+        delay: 10000,
         },
         loop: true,
     	});
