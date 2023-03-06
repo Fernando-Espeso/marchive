a635dce8bd37827a91c990ccf2c61b4f5ede3504
Fernando-Espeso <sre.fernando@gamil.com>
2023-03-06 17:21:26 +0100

    yes

--- assets/global.js
+++ assets/global.js
@@ -72,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function(){
     var didScroll;
     var lastScrollTop = 0;
     var delta = 5;
-    var navbarHeight = $('.logo').outerHeight();
+    var navbarHeight = $('#header').outerHeight();
 
     $(window).scroll(function(event){
         didScroll = true;
@@ -96,11 +96,11 @@ document.addEventListener("DOMContentLoaded", function(){
         // This is necessary so you never see what is "behind" the navbar.
         if (st > lastScrollTop && st > navbarHeight){
             // Scroll Down
-            $('.header').removeClass('nav-down').addClass('nav-up');
+            $('#header').removeClass('nav-down').addClass('nav-up');
         } else {
             // Scroll Up
             if(st + $(window).height() < $(document).height()) {
-                $('.header').removeClass('nav-up').addClass('nav-down');
+                $('#header').removeClass('nav-up').addClass('nav-down');
             }
         }
 
