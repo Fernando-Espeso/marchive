90e8402982e971a651448f5a87caf310d2aa9c72
Fernando-Espeso <sre.fernando@gamil.com>
2023-03-07 15:20:49 +0100

    es

--- assets/global.js
+++ assets/global.js
@@ -67,36 +67,36 @@ document.addEventListener("DOMContentLoaded", function(){
       $(this).toggleClass("active");
   		$("."+this.id).toggleClass("show");
     });
-    var didScroll;
-    var lastScrollTop = 0;
-    var delta = 5;
-    var navbarHeight = $('.header').outerHeight();
-    $(window).scroll(function(event){
-        didScroll = true;
-    });
-    setInterval(function() {
-        if (didScroll) {
-            hasScrolled();
-            didScroll = false;
-        }
-    }, 250);
-    function hasScrolled() {
-        var st = $(this).scrollTop();
-        if(Math.abs(lastScrollTop - st) <= delta)
-            return;
-        if (st > lastScrollTop && st > navbarHeight){
-            // Scroll Down
-            $('.header').removeClass('nav-down').addClass('nav-up');
-        }
-        else {
-            // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.header').removeClass('nav-up').addClass('nav-down');
-          }
-        }
-
-        lastScrollTop = st;
-    };
+    // var didScroll;
+    // var lastScrollTop = 0;
+    // var delta = 5;
+    // var navbarHeight = $('.header').outerHeight();
+    // $(window).scroll(function(event){
+    //     didScroll = true;
+    // });
+    // setInterval(function() {
+    //     if (didScroll) {
+    //         hasScrolled();
+    //         didScroll = false;
+    //     }
+    // }, 250);
+    // function hasScrolled() {
+    //     var st = $(this).scrollTop();
+    //     if(Math.abs(lastScrollTop - st) <= delta)
+    //         return;
+    //     if (st > lastScrollTop && st > navbarHeight){
+    //         // Scroll Down
+    //         $('.header').removeClass('nav-down').addClass('nav-up');
+    //     }
+    //     else {
+    //         // Scroll Up
+    //     if(st + $(window).height() < $(document).height()) {
+    //         $('.header').removeClass('nav-up').addClass('nav-down');
+    //       }
+    //     }
+    //
+    //     lastScrollTop = st;
+    // };
     var mySwiper = new Swiper ('.home',
     	{
         speed:0,
