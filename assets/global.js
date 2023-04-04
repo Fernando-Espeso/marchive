d8acb296dac7f56380b5901c7ac6800d8651a732
Fernando-Espeso <sre.fernando@gamil.com>
2023-04-04 12:38:05 +0200

    yes

--- assets/global.js
+++ assets/global.js
@@ -58,21 +58,21 @@ document.addEventListener("DOMContentLoaded", function(){
 
     $('#description').click(function(){
       $(".nav-info div").removeClass("active");
-      $(".show-description").hide();
+      $(".show-description").removeClass("show");
   		$("."+this.id).toggleClass("show");
     });
 
     $('#size').click(function(){
       $(".nav-info div").removeClass("active");
       $(this).toggleClass("active");
-      $(".show-description").hide();
+      $(".show-description").removeClass("show");
   		$("."+this.id).toggleClass("show");
     });
 
     $('#condition').click(function(){
       $(".nav-info div").removeClass("active");
       $(this).toggleClass("active");
-      $(".show-description").hide();
+      $(".show-description").removeClass("show");
   		$("."+this.id).toggleClass("show");
     });
     var mySwiper = new Swiper ('.home',
