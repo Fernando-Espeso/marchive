f0c09b3a66c7704b2be36cb2c9c18710f2d804cc
Fernando-Espeso <sre.fernando@gamil.com>
2023-04-04 16:18:41 +0200

    yes

--- assets/global.js
+++ assets/global.js
@@ -89,9 +89,9 @@ document.addEventListener("DOMContentLoaded", function(){
     $('.sort').click(function(){
              var t = $(this),
              k = t.data('sort');
-             t.parent().siblings().children().toggleClass('active');
+             t.parent().siblings().children().removeClass('active');
              if(!t.hasClass('active')){
-                     t.toggleClass('active');
+                     t.addClass('active');
                      $('.product:not([data-'+ k +'="null"])').sort(function(a, b){
                              if(!t.hasClass('invert')){
                                      return $(b).data(k) < $(a).data(k) ? 1 : -1;
